8964ffe858ffb77f9f4f150d72122325118c2a60
drakzone555 <77877377+drakzone555@users.noreply.github.com>
2023-03-03 08:41:27 +0700

    3323

--- Assets/Scripts/UI/LobbyMenu.cs
+++ Assets/Scripts/UI/LobbyMenu.cs
@@ -18,7 +18,7 @@ public class LobbyMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text _joinCodeText;
     [SerializeField] private TMP_InputField _joinCodeInputField;
-    // [SerializeField] private GameObject _button;
+    [SerializeField] private GameObject _button;
 
     private UnityTransport _transport;
     private const int MaxPlayers = 2;
@@ -31,19 +31,19 @@ public class LobbyMenu : MonoBehaviour
         private async void Awake()
     {
         _transport = FindObjectOfType<UnityTransport>();
-        // _button.SetActive(false);
+        _button.SetActive(false);
         await Authenticate();
-        // _button.SetActive(true);
+        _button.SetActive(true);
     }
 
     public async void CreateGame()
     {
-        // _button.SetActive(false);
+        _button.SetActive(false);
 
         Allocation a = await RelayService.Instance.CreateAllocationAsync(MaxPlayers);
         _joinCodeText.text = await RelayService.Instance.GetJoinCodeAsync(a.AllocationId);
 
-        _transport.SetHostRelayData(a.RelayServer.IpV4, 
+        _transport.SetHostRelayData(a.RelayServer.IpV4,
             (ushort)a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData);
 
         NetworkManager.Singleton.StartHost();
@@ -51,10 +51,10 @@ public class LobbyMenu : MonoBehaviour
 
     public async void JoinGame()
     {
-        // _button.SetActive(false);
+        _button.SetActive(false);
 
         JoinAllocation a = await RelayService.Instance.JoinAllocationAsync(_joinCodeInputField.text);
-        _transport.SetClientRelayData(a.RelayServer.IpV4, 
+        _transport.SetClientRelayData(a.RelayServer.IpV4,
             (ushort)a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData, a.HostConnectionData);
 
         NetworkManager.Singleton.StartClient();
